8b7a0c79a625b02ca30581c839975232df73805b
Elisabete <38789950+Ebernardinof@users.noreply.github.com>
2022-02-21 21:59:50 +0000

    first commit

---
+++ src/components/Signup.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Signup = () => {
+  return (
+    <div className="ui container">
+      <div className="ui grid stackable">
+        <div className="ui two column centered grid">
+          <div className="column">ifgnup</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Signup;
